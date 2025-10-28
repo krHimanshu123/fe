a68382759aca09248ce1175784c428f0dd9b366d
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-10-28 23:04:03 +0530

    Fix formatting in Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,6 +26,7 @@ const Login = () => {
         <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button onClick={handleLogin}>Login</button>
         <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
+        
       </div>
     </div>
   );
